328418dc6d25af1732f18bb7dca97e106f1d9281
Pete Currey <PeteCurrey@users.noreply.github.com>
2026-05-02 22:09:27 +0100

    fix: update stripe api version

--- lib/stripe/server.ts
+++ lib/stripe/server.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
-  apiVersion: '2024-06-20', // Use the latest stable or the one installed (17.3.1 supports this)
+  apiVersion: '2024-10-28.acacia',
   appInfo: {
     name: 'ForecourIQ DMS',
     version: '0.1.0',
